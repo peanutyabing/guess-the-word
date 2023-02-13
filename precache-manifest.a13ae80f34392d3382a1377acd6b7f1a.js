aa91a5984f5173c73a9ce2b9c4bfb2c2afe16d62
peanutyabing <sherry.liuyabing@gmail.com>
2023-02-13 21:21:33 +0800

    Updates

---
+++ precache-manifest.a13ae80f34392d3382a1377acd6b7f1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a8bba2b4e41d43dbdf6",
+    "url": "/react-gh-pages/static/css/main.d19bddc2.chunk.css"
+  },
+  {
+    "revision": "3a8bba2b4e41d43dbdf6",
+    "url": "/react-gh-pages/static/js/main.3a8bba2b.chunk.js"
+  },
+  {
+    "revision": "82b1a1f99de602cd3a08",
+    "url": "/react-gh-pages/static/js/1.82b1a1f9.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "e07f0fe15674d1d05d85d2ddf45ee9ef",
+    "url": "/react-gh-pages/index.html"
+  }
+];
