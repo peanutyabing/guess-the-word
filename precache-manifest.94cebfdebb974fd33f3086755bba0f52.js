3879e8545b1c50d0d95c0d1ec0395a6e585eb6be
peanutyabing <sherry.liuyabing@gmail.com>
2023-02-13 21:22:30 +0800

    Updates

---
+++ precache-manifest.94cebfdebb974fd33f3086755bba0f52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a8bba2b4e41d43dbdf6",
+    "url": "/guess-the-word/static/css/main.d19bddc2.chunk.css"
+  },
+  {
+    "revision": "3a8bba2b4e41d43dbdf6",
+    "url": "/guess-the-word/static/js/main.3a8bba2b.chunk.js"
+  },
+  {
+    "revision": "82b1a1f99de602cd3a08",
+    "url": "/guess-the-word/static/js/1.82b1a1f9.chunk.js"
+  },
+  {
+    "revision": "442850cea056c6a021bb",
+    "url": "/guess-the-word/static/js/runtime~main.442850ce.js"
+  },
+  {
+    "revision": "2cc5356b4c68f5b559c2553f8c5079d2",
+    "url": "/guess-the-word/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.a13ae80f34392d3382a1377acd6b7f1a.js"
+  "/guess-the-word/precache-manifest.94cebfdebb974fd33f3086755bba0f52.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/guess-the-word/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
